b9f0865602e6574171fbac26b5690e5ba4d884ec
Wayne Taylor <wayne@kryptos.co.uk>
2021-08-17 23:14:09 +0100

    init commit

---
+++ Controllers/ServerConfigController.cs
@@ -0,0 +1,107 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RedStone.Controllers
+{
+    public class ServerConfig : Controller
+    {
+        // GET: ServerConfig
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: ServerConfig/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: ServerConfig/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ServerConfig/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+
+                //Filename 
+                string filename = "server.properties";
+                
+                // Text 
+                string gamemode = "gamemode=" + collection["gamemode"];
+                string allow_cheats = "allow-cheats=" + collection["allow_cheats"];
+
+                // Set a variable to the Documents path.
+                string docPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+
+                // Write the string array to a new file named "WriteLines.txt".
+                using (StreamWriter outputFile = new StreamWriter(Path.Combine(docPath, filename)))
+                {
+                   outputFile.WriteLine(gamemode);
+                   outputFile.WriteLine(allow_cheats);
+                }
+
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ServerConfig/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: ServerConfig/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ServerConfig/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: ServerConfig/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Models/ServerProperties.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RedStone.Models
+{
+    public class ServerProperties
+    {
+
+        public string gamemode { get; set; }
+        public string force_gamemode { get; set; }
+        public string difficulty { get; set; }
+        public string allow_cheats { get; set; }
+        public string max_players { get; set; }
+        public string online_mode { get; set; }
+        public string white_list { get; set; }
+        public string server_port { get; set; }
+        public string server_portv6 { get; set; }
+        public string view_distance { get; set; }
+        public string tick_distance { get; set; }
+        public string player_idle_timeout { get; set; }
+        public string max_threads { get; set; }
+        public string level_name { get; set; }
+        public string level_seed { get; set; }
+        public string default_layer_permission_level { get; set; }
+        public string texturepack_required { get; set; }
+        public string content_log_file_enabled { get; set; }
+        public string compression_threshold { get; set; }
+        public string server_authoritative_movement { get; set; }
+        public string player_movement_score_threshold { get; set; }
+        public string player_movement_distance_threshold { get; set; }
+        public string player_movement_duration_threshold_in_ms { get; set; }
+        public string correct_player_movement {get;set;}
+        public string server_authoritative_block_breaking { get; set; }
+ }
+}
